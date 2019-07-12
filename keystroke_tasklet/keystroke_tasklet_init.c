16b897bcdf56cf42ef1b1edd0da5b4dab1662870
shashank.arora07 <shashank.arora07@gmail.com>
2019-07-12 20:46:13 +0530

    updated

--- keystroke_tasklet/keystroke_tasklet_init.c
+++ keystroke_tasklet/keystroke_tasklet_init.c
@@ -2,14 +2,44 @@
 
 char keystroke_device[] = "keystroke";
 
-DECLARE_TASKLET(keystroke_tasklet,keystroke_tasklet_handler,)
-
-irq_handler_t keystroke_irq_handler(int irq, void *drvdata)
+static void keystroke_tasklet_handler(unsigned long data)
 {
-	pr_notice("hardIRQ handler called! pid = %d\n",current->pid);
-	pr_notice("In hard IRQ context: %lu \n",in_irq());
-	pr_notice("In soft IRQ context: %lu \n",in_softirq());
-	pr_notice("In interrupt context: %lu \n",in_interrupt());
+	unsigned status;
+	unsigned scancode;
+
+	pr_notice("keystroke_tasklet_handler called!! PID = %u\n",current->pid);
+	status = inb(0x64);
+	scancode = inb(0x60);
+
+	pr_notice("Caught Scancode = %u and Status = %u\n",scancode,status);	
+	pr_notice("keystroke_tasklet_handler: hard IRQ context: %lu \n",in_irq());
+	pr_notice("keystroke_tasklet_handler: soft IRQ context: %lu \n",in_softirq());
+	pr_notice("keystroke_tasklet_handler: interrupt context: %lu \n",in_interrupt());
+	pr_notice("----------------------------------------");
+	dump_stack();	
+//	mdelay(1000);
+	pr_notice("----------------------------------------");
+
+	pr_notice("keystroke_tasklet_handler finished.....\n");
+}
+
+
+DECLARE_TASKLET(keystroke_tasklet,keystroke_tasklet_handler,0);
+
+/*
+	#define DECLARE_TASKLET(name,func,data) \
+	struct tasklet_struct keystroke_tasklet = 
+	{ func: keystroke_taskler_handler,
+	  data: (unsigned long) keystroke_device
+	}
+*/
+
+irqreturn_t keystroke_irq_handler(int irq, void *drvdata)
+{
+	pr_notice("keystroke_irq_handler called! pid = %d\n",current->pid);
+	pr_debug("keystroke_irq_handler hard IRQ context: %lu \n",in_irq());
+	pr_debug("keystroke_irq_handler soft IRQ context: %lu \n",in_softirq());
+	pr_debug("keystroke_irq_handler interrupt context: %lu \n",in_interrupt());
 
 	tasklet_schedule(&keystroke_tasklet);
 	
@@ -33,3 +63,9 @@ out:
 	return ret;
 
 }
+
+module_init(keystroke_init);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Shashank Arora");
+MODULE_DESCRIPTION("A keystroke tasklet handler module: prints scancode on keypress/release");
